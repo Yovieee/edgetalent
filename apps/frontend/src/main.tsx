0098ea51456f86ccf29ad548b453c1d4b447b291
Yovieee <inimemangyonas@gmail.com>
2026-07-23 17:03:56 +0700

    perbaiki url

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -3,14 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { SupabaseProvider } from './context/SupabaseProvider.tsx'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <SupabaseProvider>
-      <HashRouter>
+      <BrowserRouter>
         <App />
-      </HashRouter>
+      </BrowserRouter>
     </SupabaseProvider>
   </StrictMode>,
 )
